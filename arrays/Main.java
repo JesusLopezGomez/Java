59b7745660c2429d5aa88e13d59ae44c443a344b
lopeeh <jelopez.gom@gmail.com>
2023-03-01 14:20:15 +0100

    actualización arrays

--- arrays/Main.java
+++ arrays/Main.java
@@ -23,11 +23,14 @@ public class Main {
 		//for(int i = 0; i < numeros2.length; i++) {
 			//System.out.println("Introduce el número de la posición " + (i+1) +"/10:");
 		//}
-
-		for(int j = 0; j < numeros2.length; j++) {
-			
-			vectorDesplazado.append(numeros2[j]);
-		}System.out.println(vectorOriginal.toString());
+        int i;
+        int aux = numeros2.length-1;    
+        vectorDesplazado.append(aux);
+        for (i = (numeros2.length-1); i > 0; i--) {
+        	numeros2[i] = numeros2[i-1];
+			vectorDesplazado.append(numeros2[i]);
+		}
+        System.out.println(vectorOriginal.toString());
 		System.out.println(vectorDesplazado.toString());
 		
 	}
