910d267016d557502ce9921f003e4ada8f14224e
lopeeh <jelopez.gom@gmail.com>
2023-05-09 14:22:39 +0200

    más test parametrizados

--- boletin1EstructuraDatos/chatInstituto_ej5/testAlumno/TestAlumno.java
+++ boletin1EstructuraDatos/chatInstituto_ej5/testAlumno/TestAlumno.java
@@ -1,10 +1,11 @@
 package boletin1EstructuraDatos.chatInstituto_ej5.testAlumno;
 
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 
-import org.junit.jupiter.api.Test;
+import java.io.File;
+
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -15,57 +16,79 @@ import boletin1EstructuraDatos.chatInstituto_ej5.model.Profesor;
 
 class TestAlumno {
 
-	Alumno a2 = new Alumno("pepe",19);
 	Alumno a3 = new Alumno("jose",29);
 	Profesor p = new Profesor("Jose manuel",45);
 	
 	@ParameterizedTest
-	@CsvSource(value = {"Manuel,17","Jorge,19","Juan,20"})
+	@CsvSource(value = {"alumno,17","profesor,19","alumno,20","profesor,15"})
 	void testEnviarMensajeCsvSource(String nombre, String edad) {
-		Alumno a = new Alumno(nombre, Integer.valueOf(edad));
-		try {
-			a.enviarMensaje(p, "No entiendo el ejercicio 99");
-		}catch(ExceptionsPersona e) {
-			assert(false);
-		}
-		
-		try {
-			a.enviarMensaje(a, "que haces");
-		} catch (ExceptionsPersona e) {
-			assert(true);
-		}
-		
+		Alumno a = new Alumno("pepe",Integer.valueOf(edad));
+		Alumno a2 = null;
+		Profesor p2 = null;
+	
+		if(nombre.equals("alumno")) {
+			a2 = new Alumno("pepito",Integer.valueOf(edad));
+			try {
+				a.enviarMensaje(a2, "que haces");
+			} catch (ExceptionsPersona e) {
+				assert(true);
+			}
+		}else {
+			p2 = new Profesor("pepon",Integer.valueOf(edad));
+			try {
+				a.enviarMensaje(p2, "No entiendo el ejercicio 99");
+			}catch(ExceptionsPersona e) {
+				assert(false);
+			}
+		}			
+
 	} 
 	
 	@ParameterizedTest
-	@ValueSource(strings = {"Hola que tal","que haces","de locos"})
-	void testLeerMensajesBuzonValueSource(String mensaje) {
-		a2.recibirMensaje(new Mensaje(a2, mensaje));
-		try {
-			a2.leerMensajesBuzon().isEmpty();
-		} catch (ExceptionsPersona e) {
-			assert(false);
-		}
+	@CsvFileSource(files = {"./files/testEnviarMensajeAlumno.csv"})
+	void testEnviarMensajeCsvFile(String nombre, String edad) {
+		Alumno a = new Alumno("pepe",Integer.valueOf(edad));
+		Alumno a2 = null;
+		Profesor p2 = null;
 		
+		if(nombre.equals("alumno")) {
+			a2 = new Alumno("pepito",Integer.valueOf(edad));
+			try {
+				a.enviarMensaje(a2, "que haces");
+			} catch (ExceptionsPersona e) {
+				assert(true);
+			}
+		}else {
+			p2 = new Profesor("pepon",Integer.valueOf(edad));
+			try {
+				a.enviarMensaje(p2, "No entiendo el ejercicio 99");
+			}catch(ExceptionsPersona e) {
+				assert(false);
+			}
+		}			
+
+	} 
+	
+	@ParameterizedTest
+	@ValueSource(strings = {"Hola que tal","que haces",""})
+	void testLeerMensajesBuzonValueSource(String mensaje) {
+		
+		if(!mensaje.isEmpty()) {
+			a3.recibirMensaje(new Mensaje(a3, mensaje));	
+		}
 		try {
 			a3.leerMensajesBuzon();
 		} catch (ExceptionsPersona e) {
 			assert(true);
-		}
-		
-		
-		
+		}		
 	}
 
 	@ParameterizedTest
-	@ValueSource(strings = {"que haces xulon","haciendo test","flama"})
+	@ValueSource(strings = {"que haces xulon","haciendo test",""})
 	void testLeerMensajesBuzonOrdenadorValueSource(String mensaje) {
-		a2.recibirMensaje(new Mensaje(a2,mensaje));
 		
-		try {
-			a2.leerMensajesBuzonOrdenadorAlfabeticamente();
-		} catch (ExceptionsPersona e) {
-			assert(false);
+		if(!mensaje.isEmpty()) {
+			a3.recibirMensaje(new Mensaje(a3,mensaje));
 		}
 		
 		try {
@@ -80,12 +103,12 @@ class TestAlumno {
 	@ParameterizedTest
 	@ValueSource(strings = {"haciendo test","flama"})
 	void borrarMensajeBuzonValueSource(String mensaje) {
-		a2.recibirMensaje(new Mensaje(a2,mensaje));
+		a3.recibirMensaje(new Mensaje(a3,mensaje));
 		
-		a2.borrarMensajeBuzon(3);
+		a3.borrarMensajeBuzon(3);
 
 		try {
-			assertFalse(a2.leerMensajesBuzon().isEmpty());
+			assertFalse(a3.leerMensajesBuzon().isEmpty());
 		} catch (ExceptionsPersona e) {
 			assert(false);
 		}
@@ -95,9 +118,9 @@ class TestAlumno {
 	@ParameterizedTest
 	@ValueSource(strings = {"Hola fran", "que tal","gg","bien"})
 	void encontrarMensajeContieneFrase(String mensaje) {
-		a2.recibirMensaje(new Mensaje(a2,mensaje));
+		a3.recibirMensaje(new Mensaje(a3,mensaje));
 		
-		assertFalse(a2.encontrarMensajeContieneFrase(mensaje).isEmpty());
+		assertFalse(a3.encontrarMensajeContieneFrase(mensaje).isEmpty());
 	}
 
 	
